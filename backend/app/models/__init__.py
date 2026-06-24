0540e3a15f89e9d104724ee265be1ed4d6bbead0
Vikas saini <119332490+vikassaini77@users.noreply.github.com>
2026-06-24 17:58:21 +0530

    chore: export MultimodalFusionModel from models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,3 +2,4 @@
 from .sensor_tower import SensorTower
 from .two_tower import TwoTowerAnomalyModel
 from .gnn_layer import HeteroEquipmentGNN, FaultPredictionHead
+from .cross_attention import MultimodalFusionModel
